79918125aacf830ce3bc0a16b5f557493598d68f
Paul <paulmlee7@gmail.com>
2018-02-20 12:48:33 -0800

    completed navbar changes with logout

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {signOut} from '../actions';
 
 const Nav = props => {
+
+    function renderLinks(){
+        if(props.auth){
+            return (
+                <li>
+                    <button onClick={props.signOut} className='btn green'>Logout</button>
+                </li>
+            )
+        }
+
+        return [
+            <li key='0'>
+                <Link to='/sign-in'>Sign In</Link>
+            </li>,
+            <li key='1'>
+                <Link to='/sign-up'>Sign Up</Link>
+            </li>
+        ]
+    }
+
     return (
         <nav className='green'>
             <Link style={{marginLeft: '8px'}} to='/' className='brand-logo'>Movie Quotes</Link>
@@ -10,17 +32,18 @@ const Nav = props => {
                     <Link to='/about-us'>About us</Link>
                 </li>
                 <li>
-                    <Link to='/movie-quote'>Movie Quote</Link>
+                    <Link to='/movie-quote'>Movie Quoteee</Link>
                 </li>
-                <li>
-                    <Link to='/sign-in'>Sign In</Link>
-                </li>
-                <li>
-                    <Link to='/sign-up'>Sign Up</Link>
-                </li>
+                {renderLinks()}
             </ul>
         </nav>
     )
 }
 
-export default Nav;
+function mapStateToProps(state){
+    return {
+        auth: state.user.auth
+    }
+}
+
+export default connect(mapStateToProps, {signOut: signOut})(Nav);
